8be52240b873b06a816874073f456a67b2dd6513
Michael Kirsche <jnredotter@gmail.com>
2019-03-12 14:14:28 -0400

    Added script for getting delta file of an assembly compared to GrCh38

---
+++ delta.sh
@@ -0,0 +1,8 @@
+minimappath='~/github/minimap2/minimap2'
+genomepath='/home-3/mkirsche@jhu.edu/csdata/genome.fa'
+
+assembly=$1
+outprefix=$2
+
+$minimappath -x asm5 -c --cs $genomepath $assembly > $outprefix'.paf'
+python paf2delta.py $outprefix'.paf'
