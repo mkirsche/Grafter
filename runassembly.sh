1ef2c7d30e88f46fbb2dc1eb2c5abb233fb732ea
Michael Kirsche <jnredotter@gmail.com>
2019-03-18 22:18:34 -0400

    Updated file name in assembly test

--- runassembly.sh
+++ runassembly.sh
@@ -1,6 +1,6 @@
 rm /scratch/groups/mschatz1/mkirsche/ultralong/ccs/maternal*_usefulmap2.paf
 rm /scratch/groups/mschatz1/mkirsche/ultralong/*_usefulmap2.paf
-./run2.sh /scratch/groups/mschatz1/mkirsche/ultralong/ccs/maternal_and_unknown.contigs.mmpoa.fa /scratch/groups/mschatz1/mkirsche/ultralong/rel2wt_50kplus.ctg.fa /scratch/groups/mschatz1/mkirsche/ultralong/ccs/MaternalToAssembly.paf /scratch/groups/mschatz1/mkirsche/ultralong/ccs/maternal_and_unknown.contigs.mmpoa.scaffoldsassemblygraph.fa | tee maternal.log;
+./run2.sh /scratch/groups/mschatz1/mkirsche/ultralong/ccs/maternal_and_unknown.contigs.mmpoa.fa /scratch/groups/mschatz1/mkirsche/ultralong/rel2wt_50kplus.ctg.fa /scratch/groups/mschatz1/mkirsche/ultralong/ccs/AssemblyToMaternal.paf /scratch/groups/mschatz1/mkirsche/ultralong/ccs/maternal_and_unknown.contigs.mmpoa.scaffoldsassemblygraph.fa | tee maternal.log;
 
 java -cp ~/hashing/CCS/assembly_eval/ AssemblyStats /scratch/groups/mschatz1/mkirsche/ultralong/ccs/maternal_and_unknown.contigs.mmpoa.scaffoldsassemblygraph.fa | tee maternal_afterstatsassemblygraph.txt;
 
