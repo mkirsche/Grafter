6ed3db3b0a5649d95d09182f2d6bd87a105c9c34
Michael Kirsche <jnredotter@gmail.com>
2019-03-22 10:00:14 -0400

    Updated genome file path

--- delta.sh
+++ delta.sh
@@ -1,5 +1,5 @@
 minimappath=/scratch/groups/mschatz1/mkirsche/github/minimap2/minimap2
-genomepath=/home-3/mkirsche@jhu.edu/csdata/genome.fa
+genomepath=/scratch/groups/mschatz1/mkirsche/svdb/genome.fa
 
 assembly=$1
 outprefix=$2
