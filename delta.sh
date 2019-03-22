e82457952fa35b5f297a3a9ce1905fa2de80fd58
Michael Kirsche <jnredotter@gmail.com>
2019-03-22 13:42:49 -0400

    Updated minimap parameters

--- delta.sh
+++ delta.sh
@@ -4,5 +4,5 @@ genomepath=/scratch/groups/mschatz1/mkirsche/svdb/genome.fa
 assembly=$1
 outprefix=$2
 
-$minimappath -t 32 -x asm5 -c --cs $genomepath $assembly > $outprefix'.paf'
+$minimappath -t 32 -k 19 -w 19 -x asm5 -c --cs $genomepath $assembly > $outprefix'.paf'
 python paf2delta.py $outprefix'.paf'
