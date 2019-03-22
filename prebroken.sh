1686a08c494e04186da5ada5bde9ca0f693c72ec
Michael Kirsche <jnredotter@gmail.com>
2019-03-22 16:44:57 -0400

    Updated output log file name

--- prebroken.sh
+++ prebroken.sh
@@ -1,4 +1,4 @@
-./run2.sh /scratch/groups/mschatz1/mkirsche/ultralong/ccs/maternal_and_unknown.contigs.mmpoa.intra.chimera.broken.fa /scratch/groups/mschatz1/mkirsche/ultralong/rel2_200kplus.fastq /scratch/groups/mschatz1/mkirsche/ultralong/ccs/brokenmaternal.paf /scratch/groups/mschatz1/mkirsche/ultralong/ccs/maternal_and_unknown.contigs.mmpoa.broken.fa 0 2>&1 | tee maternal.log;
+./run2.sh /scratch/groups/mschatz1/mkirsche/ultralong/ccs/maternal_and_unknown.contigs.mmpoa.intra.chimera.broken.fa /scratch/groups/mschatz1/mkirsche/ultralong/rel2_200kplus.fastq /scratch/groups/mschatz1/mkirsche/ultralong/ccs/brokenmaternal.paf /scratch/groups/mschatz1/mkirsche/ultralong/ccs/maternal_and_unknown.contigs.mmpoa.broken.fa 0 2>&1 | tee brokenmaternal.log;
 
 java -cp ~/hashing/CCS/assembly_eval/ AssemblyStats /scratch/groups/mschatz1/mkirsche/ultralong/ccs/maternal_and_unknown.contigs.mmpoa.broken.fa | tee maternal_afterstatsbroken.txt;
 
