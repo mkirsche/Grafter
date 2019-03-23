49aa7ae4ee40dc8b81cd0501a234c5fa0346f8dc
Michael Kirsche <jnredotter@gmail.com>
2019-03-23 16:34:47 -0400

    Bug fix

--- run2_maternal.sh
+++ run2_maternal.sh
@@ -1,5 +1,6 @@
 WORKINGDIR=`pwd`
 OUTDIR=$WORKINGDIR'/'$1
+mkdir $OUTDIR
 BINDIR=`dirname $(readlink -f "$0")`
 
 $BINDIR/run2.sh /scratch/groups/mschatz1/mkirsche/ultralong/ccs/maternal_and_unknown.contigs.mmpoa.fa /scratch/groups/mschatz1/mkirsche/ultralong/rel2_200kplus.fastq /scratch/groups/mschatz1/mkirsche/ultralong/ccs/rel2_200kplus_ccs_mat.paf $OUTDIR/maternal_and_unknown.contigs.mmpoa.scaffoldsgraph.fa 1 2>&1 | tee $OUTDIR'/'maternal.log $OUTDIR
