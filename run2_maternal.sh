334803abfbcf443a06d7d16864255ebe997c4ba1
Michael Kirsche <jnredotter@gmail.com>
2019-03-23 23:38:28 -0400

    Bug fix

--- run2_maternal.sh
+++ run2_maternal.sh
@@ -4,7 +4,7 @@ rm -r $OUTDIR
 mkdir $OUTDIR
 BINDIR=`dirname $(readlink -f "$0")`
 
-$BINDIR/run2.sh /scratch/groups/mschatz1/mkirsche/ultralong/ccs/maternal_and_unknown.contigs.mmpoa.fa /scratch/groups/mschatz1/mkirsche/ultralong/rel2_200kplus.fastq /scratch/groups/mschatz1/mkirsche/ultralong/ccs/rel2_200kplus_ccs_mat.paf $OUTDIR/maternal_and_unknown.contigs.mmpoa.scaffoldsgraph.fa 0 $OUTDIR 2>&1 | tee $OUTDIR'/'maternal.log
+$BINDIR/run2.sh /scratch/groups/mschatz1/mkirsche/ultralong/ccs/maternal_and_unknown.contigs.mmpoa.fa /scratch/groups/mschatz1/mkirsche/ultralong/rel2_200kplus.fastq /scratch/groups/mschatz1/mkirsche/ultralong/ccs/rel2_200kplus_ccs_mat.paf $OUTDIR/maternal_and_unknown.contigs.mmpoa.scaffoldsgraph.fa 1 $OUTDIR 2>&1 | tee $OUTDIR'/'maternal.log
 
 java -cp ~/hashing/CCS/assembly_eval/ AssemblyStats $OUTDIR'/'maternal_and_unknown.contigs.mmpoa.scaffoldsgraph.fa | tee $OUTDIR'/'maternal_afterstatsgraph.txt;
 
