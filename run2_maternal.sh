90d21b03be8c51732c6e5814bcf4fcb294abbee7
mkirsche <jnredotter@gmail.com>
2019-03-25 10:31:22 -0400

    Allowed different lengths

--- run2_maternal.sh
+++ run2_maternal.sh
@@ -3,8 +3,8 @@ OUTDIR=$WORKINGDIR'/'$1
 rm -r $OUTDIR
 mkdir $OUTDIR
 BINDIR=`dirname $(readlink -f "$0")`
-
-$BINDIR/run2.sh /scratch/groups/mschatz1/mkirsche/ultralong/ccs/maternal_and_unknown.contigs.mmpoa.fa /scratch/groups/mschatz1/mkirsche/ultralong/rel2_200kplus.fastq /scratch/groups/mschatz1/mkirsche/ultralong/ccs/rel2_200kplus_ccs_mat.paf $OUTDIR/maternal_and_unknown.contigs.mmpoa.scaffoldsgraph.fa 1 $OUTDIR 2>&1 | tee $OUTDIR'/'maternal.log
+len=100
+$BINDIR/run2.sh '/scratch/groups/mschatz1/mkirsche/ultralong/ccs/maternal_and_unknown.contigs.mmpoa.fa /scratch/groups/mschatz1/mkirsche/ultralong/rel2_i'$len'kplus.fastq' '/scratch/groups/mschatz1/mkirsche/ultralong/ccs/rel2_'$len'kplus_ccs_mat.paf' $OUTDIR/maternal_and_unknown.contigs.mmpoa.scaffoldsgraph.fa 1 $OUTDIR 2>&1 | tee $OUTDIR'/'maternal.log
 
 java -cp ~/hashing/CCS/assembly_eval/ AssemblyStats $OUTDIR'/'maternal_and_unknown.contigs.mmpoa.scaffoldsgraph.fa | tee $OUTDIR'/'maternal_afterstatsgraph.txt;
 
