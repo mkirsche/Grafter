02a35d5184656e845d96f8f09d24c1c4aac3f403
Michael Kirsche <jnredotter@gmail.com>
2019-03-26 12:01:19 -0400

    Added minimap to downsampling and refactored script

--- maternal_downsample.sh
+++ maternal_downsample.sh
@@ -6,14 +6,21 @@ BINDIR=`dirname $(readlink -f "$0")`
 len=100
 prop=$2
 
-echo 'Sampling reads'
 readfn='/scratch/groups/mschatz1/mkirsche/ultralong/rel2_'$len'kplus.fastq'
+contigsfn='/scratch/groups/mschatz1/mkirsche/ultralong/ccs/maternal_and_unknown.contigs.mmpoa.fa'
+outfn=$OUTDIR'/maternal_and_unknown.contigs.mmpoa.scaffoldsgraph.fa'
 
-/scratch/groups/mschatz1/mkirsche/github/seqtk/seqtk sample $readfn $prop > $OUTDIR'/filteredreads.fastq'
+echo 'Sampling reads'
+filteredreadfn=$OUTDIR'/filteredreads.fastq'
+/scratch/groups/mschatz1/mkirsche/github/seqtk/seqtk sample $readfn $prop > $filteredreadfn
 
-$BINDIR/run2.sh /scratch/groups/mschatz1/mkirsche/ultralong/ccs/maternal_and_unknown.contigs.mmpoa.fa $OUTDIR'/filteredreads.fastq'  '/scratch/groups/mschatz1/mkirsche/ultralong/ccs/rel2_'$len'kplus_ccs_mat.paf' $OUTDIR/maternal_and_unknown.contigs.mmpoa.scaffoldsgraph.fa 1 $OUTDIR 2>&1 | tee $OUTDIR'/'maternal.log
+echo 'Aligning with minimap2'
+paffn=$OUTDIR'/alignments.paf'
+$minimappath -t 32 -k 19 -w 19 $contigsfn $filteredreadfn  > $paffn
 
-java -cp ~/hashing/CCS/assembly_eval/ AssemblyStats $OUTDIR'/'maternal_and_unknown.contigs.mmpoa.scaffoldsgraph.fa | tee $OUTDIR'/'maternal_afterstatsgraph.txt;
+$BINDIR/run2.sh $contigsfn $OUTDIR'/filteredreads.fastq'  $paffn $outfn 1 $OUTDIR 2>&1 | tee $OUTDIR'/'maternal.log
 
-$BINDIR/delta.sh $OUTDIR'/'maternal_and_unknown.contigs.mmpoa.scaffoldsgraph.fa $OUTDIR'/'after_maternalgraph;
+java -cp ~/hashing/CCS/assembly_eval/ AssemblyStats $outfn | tee $OUTDIR'/'maternal_afterstatsgraph.txt;
+
+$BINDIR/delta.sh $outfn $OUTDIR'/'after_maternalgraph;
 
