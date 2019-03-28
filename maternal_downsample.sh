180fc557890cea5c345b71a4456848e38670f249
Michael Kirsche <jnredotter@gmail.com>
2019-03-28 13:29:23 -0400

    Bug fix

--- maternal_downsample.sh
+++ maternal_downsample.sh
@@ -15,10 +15,11 @@ filteredreadfn=$OUTDIR'/filteredreads.fastq'
 /scratch/groups/mschatz1/mkirsche/github/seqtk/seqtk sample $readfn $prop > $filteredreadfn
 
 echo 'Aligning with minimap2'
+minimappath='/scratch/groups/mschatz1/mkirsche/github/minimap2/minimap2'
 paffn=$OUTDIR'/alignments.paf'
 $minimappath -t 32 -k 19 -w 19 $contigsfn $filteredreadfn  > $paffn
 
-$BINDIR/run2.sh $contigsfn $OUTDIR'/filteredreads.fastq'  $paffn $outfn 1 $OUTDIR 2>&1 | tee $OUTDIR'/'maternal.log
+$BINDIR/run2.sh $contigsfn $filteredreadfn $paffn $outfn 1 $OUTDIR 2>&1 | tee $OUTDIR'/'maternal.log
 
 java -cp ~/hashing/CCS/assembly_eval/ AssemblyStats $outfn | tee $OUTDIR'/'maternal_afterstatsgraph.txt;
 
