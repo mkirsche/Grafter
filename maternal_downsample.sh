42bf4ca73f9ff2fbcfbc6233d3e4dc12dc43abf2
Michael Kirsche <jnredotter@gmail.com>
2019-04-01 14:46:39 -0400

    Copied old files to make tests faster

--- maternal_downsample.sh
+++ maternal_downsample.sh
@@ -13,12 +13,16 @@ outfn=$OUTDIR'/maternal_and_unknown.contigs.mmpoa.scaffoldsgraph.fa'
 
 echo 'Sampling reads'
 filteredreadfn=$OUTDIR'/filteredreads.fastq'
-/scratch/groups/mschatz1/mkirsche/github/seqtk/seqtk sample $readfn $prop > $filteredreadfn
+#/scratch/groups/mschatz1/mkirsche/github/seqtk/seqtk sample $readfn $prop > $filteredreadfn
+sampledreads='/home-3/mkirsche@jhu.edu/github/ScaffoldingUltralong/downsample12_5/filteredreads.fastq'
+cp $sampledreads $filteredreadfn
 
 echo 'Aligning with minimap2'
 minimappath='/scratch/groups/mschatz1/mkirsche/github/minimap2/minimap2'
 paffn=$OUTDIR'/alignments.paf'
-$minimappath -t 32 -k 19 -w 19 $contigsfn $filteredreadfn  > $paffn
+#$minimappath -t 32 -k 19 -w 19 $contigsfn $filteredreadfn  > $paffn
+sampledalns='/home-3/mkirsche@jhu.edu/github/ScaffoldingUltralong/downsample12_5/alignments.paf'
+cp $sampledalns $paffn
 
 $BINDIR/run2.sh $contigsfn $filteredreadfn $paffn $outfn 1 $OUTDIR 2>&1 | tee $OUTDIR'/'maternal.log
 
