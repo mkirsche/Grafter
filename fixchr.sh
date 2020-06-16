c17906269f6b3c968e8bead1dfe71f569456daaa
mkirsche <jnredotter@gmail.com>
2020-06-15 22:30:42 -0400

    Fix source path in fixchr.sh

--- fixchr.sh
+++ fixchr.sh
@@ -11,7 +11,7 @@ readfn='/scratch/groups/mschatz1/mkirsche/rel5_'$readfilter'.fastq'
 echo 'Compiling'
 javac $BINDIR/src/*.java
 echo 'Running scaffolding'
-java -cp src Main aln_fn=$alnfn fasta_fn=$contigsfn read_fn=$readfn read_map_file=$OUTDIR/'chm13_'$chr'_'$readfilter'_'$minq'_usefulreadsmap.paf' contig_map_file=$OUTDIR/'chm13_'$chr'_'$readfilter'_'$minq'_usefulcontigsmap.paf' out_file=$newcontigsfile minq=$minq full_out_gfa_fn=$OUTDIR/'scaffolds_'$chr'_'$readfilter'_'$minq'.full.gfa' joins_out_gfa_fn=$OUTDIR/'scaffolds_'$chr'_'$readfilter'_'$minq'.joins.gfa' read_metadata_fn=$OUTDIR/'scaffolds_'$chr'_'$readfilter'_'$minq'.reads.tsv' min_weight_supp=150 min_weight=10
+java -cp $BINDIR/src Main aln_fn=$alnfn fasta_fn=$contigsfn read_fn=$readfn read_map_file=$OUTDIR/'chm13_'$chr'_'$readfilter'_'$minq'_usefulreadsmap.paf' contig_map_file=$OUTDIR/'chm13_'$chr'_'$readfilter'_'$minq'_usefulcontigsmap.paf' out_file=$newcontigsfile minq=$minq full_out_gfa_fn=$OUTDIR/'scaffolds_'$chr'_'$readfilter'_'$minq'.full.gfa' joins_out_gfa_fn=$OUTDIR/'scaffolds_'$chr'_'$readfilter'_'$minq'.joins.gfa' read_metadata_fn=$OUTDIR/'scaffolds_'$chr'_'$readfilter'_'$minq'.reads.tsv' min_weight_supp=100 min_weight=10
 echo 'Stitching in new contigs'
 java -cp $BINDIR/src StitchFasta $contigsfn $newcontigsfile $outfile
 echo 'Done'
